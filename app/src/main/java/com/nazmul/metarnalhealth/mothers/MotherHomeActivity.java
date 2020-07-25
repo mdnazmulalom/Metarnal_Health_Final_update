3b6c0bd934f7ea0d6f413a310d04fbba5460bc7e
Md.Nazmul Alom <34202639+CoderNazmulAlom@users.noreply.github.com>
2020-07-26 01:32:01 +0600

    doctor home and mother home add

---
+++ app/src/main/java/com/nazmul/metarnalhealth/mothers/MotherHomeActivity.java
@@ -0,0 +1,16 @@
+package com.nazmul.metarnalhealth.mothers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nazmul.metarnalhealth.R;
+
+public class MotherHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mother_home);
+    }
+}
