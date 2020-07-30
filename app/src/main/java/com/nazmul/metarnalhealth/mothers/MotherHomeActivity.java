c83aea5b8e38c99749c480ceaa79a75663a97a34
kausar_uddin <kausarbd22@gmailcom>
2020-07-30 20:58:16 +0600

    Home page design customize

--- app/src/main/java/com/nazmul/metarnalhealth/mothers/MotherHomeActivity.java
+++ app/src/main/java/com/nazmul/metarnalhealth/mothers/MotherHomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.nazmul.metarnalhealth.R;
 
@@ -13,7 +15,11 @@ public class MotherHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mother_home);
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.hide();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
