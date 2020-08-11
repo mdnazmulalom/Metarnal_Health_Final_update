f19326575eeaf7a8a7f6bbad56b7a25160ff5597
Md.Nazmul Alom <34202639+CoderNazmulAlom@users.noreply.github.com>
2020-08-12 00:15:50 +0600

    Update profile

--- app/src/main/java/com/nazmul/metarnalhealth/mothers/EditProfileActivity.java
+++ app/src/main/java/com/nazmul/metarnalhealth/mothers/EditProfileActivity.java
@@ -39,12 +39,12 @@ public class EditProfileActivity extends AppCompatActivity {
         String getLocation=getIntent().getExtras().getString("location");
         String getGender=getIntent().getExtras().getString("gender");
 
-
-           txt_Name.setText(getName);
-           etxt_Fullame.setText(getName);
-           etxt_Cell.setText(getCell);
-           etxt_Location.setText(getLocation);
-           etxt_Gender.setText(getGender);
+//
+//           txt_Name.setText(getName);
+//           etxt_Fullame.setText(getName);
+//           etxt_Cell.setText(getCell);
+//           etxt_Location.setText(getLocation);
+//           etxt_Gender.setText(getGender);
 
 
 
