945c8e0108629671d85f4c873bd8007b75b20508
Md.Nazmul Alom <34202639+CoderNazmulAlom@users.noreply.github.com>
2020-08-14 23:18:59 +0600

    Doctor profile Activity add

---
+++ app/src/main/java/com/nazmul/metarnalhealth/doctors/DoctorProfileActivity.java
@@ -0,0 +1,17 @@
+package com.nazmul.metarnalhealth.doctors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nazmul.metarnalhealth.R;
+
+public class DoctorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+
+    }
+}
