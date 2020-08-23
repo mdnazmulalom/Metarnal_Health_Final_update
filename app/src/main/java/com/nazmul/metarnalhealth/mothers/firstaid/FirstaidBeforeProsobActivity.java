764adf7c0d882ee7d2d8e35964b7c7befcd151f0
kausar_uddin <kausarbd22@gmailcom>
2020-08-24 00:32:26 +0600

    Update FirstaidActivity

--- app/src/main/java/com/nazmul/metarnalhealth/mothers/firstaid/FirstaidBeforeProsobActivity.java
+++ app/src/main/java/com/nazmul/metarnalhealth/mothers/firstaid/FirstaidBeforeProsobActivity.java
@@ -1,43 +1,23 @@
-package com.nazmul.metarnalhealth.mothers;
+package com.nazmul.metarnalhealth.mothers.firstaid;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 import com.nazmul.metarnalhealth.R;
-import com.nazmul.metarnalhealth.mothers.firstaid.FirstaidBeforeProsobActivity;
 
-public class FirstaidActivity extends AppCompatActivity {
-
-    CardView before_prosob;
+public class FirstaidBeforeProsobActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firstaid);
-
-        before_prosob=findViewById(R.id.beforeprosob);
+        setContentView(R.layout.activity_firstaid_before_prosob);
 
         getSupportActionBar().setHomeButtonEnabled(true); //for back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//for back button
-        getSupportActionBar().setTitle("নীড় পাতা ");
-
-
-
-        before_prosob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FirstaidActivity.this, FirstaidBeforeProsobActivity.class);
-                startActivity(intent);
-            }
-        });
+        getSupportActionBar().setTitle("প্রসব পূর্ব সেবা");
     }
-
-
 
     //for back button
     @Override
