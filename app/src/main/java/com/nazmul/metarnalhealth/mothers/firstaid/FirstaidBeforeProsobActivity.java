47a778dc7ff88d1b924376258c5830edda5a0254
kausar_uddin <kausarbd22@gmailcom>
2020-08-25 23:55:32 +0600

    prosuti mayer jotno

--- app/src/main/java/com/nazmul/metarnalhealth/mothers/firstaid/FirstaidBeforeProsobActivity.java
+++ app/src/main/java/com/nazmul/metarnalhealth/mothers/firstaid/FirstaidBeforeProsobActivity.java
@@ -1,14 +1,17 @@
 package com.nazmul.metarnalhealth.mothers.firstaid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.nazmul.metarnalhealth.R;
 
 public class FirstaidBeforeProsobActivity extends AppCompatActivity {
-
+    CardView mayerjotno,aftercheckup,babysystem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,18 @@ public class FirstaidBeforeProsobActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true); //for back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//for back button
         getSupportActionBar().setTitle("প্রসব পূর্ব সেবা");
+
+        mayerjotno=findViewById(R.id.mayer_jotno);
+        aftercheckup=findViewById(R.id.after_checkup);
+        babysystem=findViewById(R.id.baby_system);
+
+        mayerjotno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(FirstaidBeforeProsobActivity.this, Firstaid_after_1.class);
+                startActivity(i);
+            }
+        });
     }
 
     //for back button
